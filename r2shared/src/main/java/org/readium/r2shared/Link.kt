3193c9acb71823fa1c015bb0a2dad3bd2a04c40f
Clement Baumann <clement.baumann@edrlab.org>
2017-08-30 18:25:02 +0200

    First elements of r2shared

---
+++ r2shared/src/main/java/org/readium/r2shared/Link.kt
@@ -0,0 +1,25 @@
+package org.readium.r2shared
+
+/**
+ * Created by cbaumann on 30/08/2017.
+ */
+
+class Link {
+
+    var href: String? = null
+    /// MIME type of resource.
+    var typeLink: String? = null
+    /// Indicates the relationship between the resource and its containing collection.
+    var rel = emptyList<String>()
+    /// Indicates the height of the linked resource in pixels.
+    var height: Int = 0
+    /// Indicates the width of the linked resource in pixels.
+    var width: Int = 0
+    var title: String? = null
+    /// Properties associated to the linked resource.
+    //var properties: Properties!
+    /// Indicates the length of the linked resource in seconds.
+    //var duration: TimeInterval?
+    /// Indicates that the linked resource is a URI template.
+    var templated: Boolean? = false
+}
---
+++ r2shared/src/main/java/org/readium/r2shared/Metadata.kt
@@ -0,0 +1,7 @@
+/**
+ * Created by cbaumann on 30/08/2017.
+ */
+
+class Metadata{
+
+}
