cc345f1d245a5ecfb5391f1d30fb94c60fc6aafc
Aferdita Muriqi <aferdita.muriqi@gmail.com>
2017-10-03 15:29:35 +0200

    applied some source formatting

--- r2streamer/src/main/java/org/readium/r2/streamer/Fetcher/Fetcher.kt
+++ r2streamer/src/main/java/org/readium/r2/streamer/Fetcher/Fetcher.kt
@@ -1,11 +1,10 @@
 package org.readium.r2.streamer.Fetcher
 
-import org.readium.r2.shared.*;
+import org.readium.r2.shared.Publication
 import org.readium.r2.streamer.Containers.Container
 import java.io.InputStream
-import java.net.URL
 
-class Fetcher(publication: Publication, container: Container){
+class Fetcher(publication: Publication, container: Container) {
     lateinit var publication: Publication
     lateinit var container: Container
     var rootFileDirectory: String = ""
@@ -25,7 +24,7 @@ class Fetcher(publication: Publication, container: Container){
         contentFilters = getContentFilters(container.rootFile.mimetype)
     }
 
-    fun data(path: String) : ByteArray? {
+    fun data(path: String): ByteArray? {
         publication.resource(path) ?: throw Exception("Missing file")
         var data: ByteArray? = container.data(path)
         if (data != null)
@@ -33,22 +32,22 @@ class Fetcher(publication: Publication, container: Container){
         return data
     }
 
-    fun dataStream(path: String) : InputStream {
-        publication.resource(path) ?: throw Exception("Missing file")
+    fun dataStream(path: String): InputStream {
+        publication.resource("/" + path) ?: throw Exception("Missing file")
         val inputStream = container.dataInputStream(path)
-        contentFilters?.apply(inputStream, publication, path)
+//        contentFilters?.apply(inputStream, publication, path)
         return inputStream
     }
 
-    fun dataLength(path: String) : Long {
+    fun dataLength(path: String): Long {
         val relativePath = rootFileDirectory.plus(path)
 
         publication.resource(path) ?: throw Exception("Missing file")
         return container.dataLength(relativePath)
     }
 
-    fun getContentFilters(mimeType: String?) : ContentFilters {
-        when (mimeType){
+    fun getContentFilters(mimeType: String?): ContentFilters {
+        when (mimeType) {
             "application/epub+zip", "application/oebps-package+xml" -> return ContentFiltersEpub()
             "application/x-cbr" -> return ContentFiltersCbz()
             else -> throw Exception("Missing container  else MIMEtype")
