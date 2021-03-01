9a355c060429267a75f36117747801345b708c9e
Mickaël Menu <mickael.menu@gmail.com>
2021-03-01 13:29:26 +0100

    Upgrade to Readium 2.0.0-beta.2

---
+++ .idea/fileTemplates/internal/Kotlin Enum.kt
@@ -0,0 +1,12 @@
+/*
+ * Copyright $YEAR Readium Foundation. All rights reserved.
+ * Use of this source code is governed by the BSD-style license
+ * available in the top-level LICENSE file of the project.
+ */
+ 
+#if (${PACKAGE_NAME} && ${PACKAGE_NAME} != "")package ${PACKAGE_NAME}
+
+#end
+#parse("File Header.java")
+enum class ${NAME} {
+}
